35b4d935ced052c7ffd17d98e8ba649c94d079ca
Andrea Paz <46249374+AndreaToPaz@users.noreply.github.com>
2023-02-27 16:47:25 -0400

    Tailwind Working now

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <button className="btn rounded-full">One</button>
+       </div>
+    );
+}
+ 
+export default Navbar ;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,16 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import 'tailwindcss/tailwind.css';
+import Navbar from 'components/Navbar'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      Sign in and out 
+       <Navbar></Navbar>
 
     </>
   )
