2341feed19a812edeaddd23eae3027af71a41873
Andrea Paz <46249374+AndreaToPaz@users.noreply.github.com>
2023-03-02 16:31:37 -0400

    Error Fix and Navbar design donde

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
+import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-neutral text-neutral-content">
             <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <a className="btn btn-ghost normal-case text-xl">Algebra Calculator</a>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1">
-                <li><a>Item 1</a></li>
-                <li tabIndex={0}>
-                    <a>
-                    Parent
-                    <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-                    </a>
-                    <ul className="p-2 bg-base-100">
-                    <li><a>Submenu 1</a></li>
-                    <li><a>Submenu 2</a></li>
-                    </ul>
-                </li>
-                <li><a>Item 3</a></li>
+                    <Link href="/calculator"><li> <a>Calculator</a></li></Link>
+                    <li tabIndex={0}>
+                        <a>
+                            Conjunction
+                            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+                        </a>
+                        <ul className="p-2 bg-neutral">
+                                <Link href="/conjunction"> <li><a>Tutorial</a></li></Link>
+                                
+                                <Link href="/conjunction/create"> <li><a>Create / View</a></li></Link>
+                        </ul>
+                        </li>
+                    <Link href="/about"><li><a>About</a></li></Link>
                 </ul>
             </div>
          </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,17 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import 'tailwindcss/tailwind.css';
+import React from 'react'
 import Navbar from 'components/Navbar'
+
 
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-       <Navbar></Navbar>
-
-    </>
+    <div>
+      <Navbar></Navbar>
+    </div>
   )
 }
