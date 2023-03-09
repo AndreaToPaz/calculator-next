dc5bbe360c0fe0368b11a28f3d3c17127289ca6d
Andrea Paz <46249374+AndreaToPaz@users.noreply.github.com>
2023-03-09 16:44:33 -0400

    Home Page design

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,14 +10,19 @@ import Display from '@/components/Display';
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <div>
-      <Navbar></Navbar>
-
-      <div className='bg-base-300'>
-        <p className='text-center text-xl'>Calculadora de expresiones algebraicas</p>
+   return (
+      <div className='h-screen bg-base-300'>
+         <Navbar></Navbar>
+         <div className='grid gap-5'>
+            <p className='mt-40 text-center text-4xl'>Calculadora de expresiones algebraicas</p>
+            <p className='ml-60 mr-60 text-center'>
+               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea saepe mollitia odit voluptatibus consectetur odio adipisci fugit commodi laborum. Necessitatibus, debitis nemo alias tempora dolores dolore eveniet tempore aut quasi?
+            </p>
+            <p className=' text-center text-6xl'>Foto</p>
+            <div className='flex justify-center'>
+               <button className="btn btn-outline btn-wide">Iniciar</button>
+            </div>
+         </div>
       </div>
-
-    </div>
-  )
+   )
 }
